5248ad58e58bf5fe6526b6cdfa12076dd8ac233a
risqi2 <risqiikhsani12@gmail.com>
2023-04-06 18:36:47 +0700

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,82 @@
 // import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import {
   QueryClient,
-  QueryClientProvider,
-  useQuery,
+  QueryClientProvider
 } from "@tanstack/react-query";
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import createEmotionCache from '../src/createEmotionCache';
+import { roboto } from './_document';
 
-export default function App({ Component, pageProps }: AppProps) {
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+
+export const defaultColor = {
+  primary: {
+    main: '#556cd6',
+  },
+  secondary: {
+    main: '#19857b',
+  },
+  error: {
+    main: red.A400,
+  },
+}
+
+export default function App(props: MyAppProps) {
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      ...defaultColor
+    },
+    typography: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    components: {
+      // Name of the component ⚛️
+      MuiButtonBase: {
+        defaultProps: {
+          // The default props to change
+          disableRipple: true, // No more ripple, on the whole application 💣!
+        },
+      },
+      MuiToolbar: {
+        styleOverrides: {
+          root: {
+            height: '70px',
+          },
+        },
+      },
+    },
+  });
+
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </QueryClientProvider>
   )
 }
